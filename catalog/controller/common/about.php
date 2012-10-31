1dc715111f10f23a57fde7e520fe9170abe4c7e9
Karmaon <karmaon@fastmail.fm>
2012-10-30 23:30:59 -0500

    Fix naming for about page

--- catalog/controller/common/about.php
+++ catalog/controller/common/about.php
@@ -1,11 +1,11 @@
 <?php  
 class ControllerCommonAbout extends Controller {
 	public function index() {
-		$this->document->setTitle($this->config->get('config_title'));
+		$this->document->setTitle('About Us');
 		$this->document->setDescription($this->config->get('config_meta_description'));
 		$this->document->setBodyId('about');
 
-		$this->data['heading_title'] = $this->config->get('config_title');
+		$this->data['heading_title'] = $this->config->get('About Us');
 		
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/about.tpl')) {
 			$this->template = $this->config->get('config_template') . '/template/common/about.tpl';
